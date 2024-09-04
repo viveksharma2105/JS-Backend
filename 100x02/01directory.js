20f2a3d36b5a40d2f5e1daf44d1119cae4140d3c
Vivek Sharma <viveksharma02005@gmail.com>
2024-09-04 08:45:03 +0530

    Init

---
+++ 100x02/01directory.js
@@ -0,0 +1,3 @@
+console.log(__dirname); //print directory name
+const path = require("path");
+console.log(path.join(__dirname,"../../01.js","/projects"));//use to give the final path or join path
---
+++ 100x02/ownCli.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const {Command} = require('commander');
+const program = new Command();
+
+
+program
+.name('File related');
+.description('CLI todo filr based task');
+.version('0.8.0');
+
