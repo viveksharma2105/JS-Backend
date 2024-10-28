ba134bf61d5a5beacc8a4959250b9a2bc78b73f4
Vivek Sharma <viveksharma02005@gmail.com>
2024-10-28 16:11:16 +0530

    first server

--- indx.js
+++ indx.js
@@ -1,12 +1,43 @@
 require('dotenv').config() //for env file (help in hiding senstive  info)
 const express = require ('express'); // express import
 const app  = express() //app variable
+
+
 const  port = 4000; // port number
 
+//github API 
+const githubdata = {
+    
+    "login": "mojombo",
+    "id": 1,
+    "node_id": "MDQ6VXNlcjE=",
+    "avatar_url": "https://avatars.githubusercontent.com/u/1?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/mojombo",
+    "html_url": "https://github.com/mojombo",
+    "followers_url": "https://api.github.com/users/mojombo/followers",
+    "following_url": "https://api.github.com/users/mojombo/following{/other_user}",
+    "gists_url": "https://api.github.com/users/mojombo/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/mojombo/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/mojombo/subscriptions",
+    "organizations_url": "https://api.github.com/users/mojombo/orgs",
+    "repos_url": "https://api.github.com/users/mojombo/repos",
+    "events_url": "https://api.github.com/users/mojombo/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/mojombo/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false
+  }
+ 
 
 //request to run (cb function, req&res is fix)
 app.get('/',  (req, res)=>{
     res.send('Hello World')
+})
+
+
+app.get('/github',  (req, res)=>{
+    res.json(githubdata)
 })
 
 app.get('/twitter', (req, res)=>{
